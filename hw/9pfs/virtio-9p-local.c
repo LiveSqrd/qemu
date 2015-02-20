d875fd5846be3a4140a2492b4ae80a536d806f11
Nathan Zadoks <nathan@nathan7.eu>
2015-02-20 13:58:08 -0500

    fixin' stuff

--- hw/9pfs/virtio-9p-local.c
+++ hw/9pfs/virtio-9p-local.c
@@ -256,28 +256,28 @@ static int local_set_xattr(const char *path, FsCred *credp)
 
     if (credp->fc_uid != -1) {
         uint32_t tmp_uid = cpu_to_le32(credp->fc_uid);
-        err = setxattr(path, "user.virtfs.uid", &tmp_uid, sizeof(uid_t), 0, 0, 0);
+        err = setxattr(path, "user.virtfs.uid", &tmp_uid, sizeof(uid_t), 0, 0);
         if (err) {
             return err;
         }
     }
     if (credp->fc_gid != -1) {
         uint32_t tmp_gid = cpu_to_le32(credp->fc_gid);
-        err = setxattr(path, "user.virtfs.gid", &tmp_gid, sizeof(gid_t), 0, 0, 0);
+        err = setxattr(path, "user.virtfs.gid", &tmp_gid, sizeof(gid_t), 0, 0);
         if (err) {
             return err;
         }
     }
     if (credp->fc_mode != -1) {
         uint32_t tmp_mode = cpu_to_le32(credp->fc_mode);
-        err = setxattr(path, "user.virtfs.mode", &tmp_mode, sizeof(mode_t), 0, 0, 0);
+        err = setxattr(path, "user.virtfs.mode", &tmp_mode, sizeof(mode_t), 0, 0);
         if (err) {
             return err;
         }
     }
     if (credp->fc_rdev != -1) {
         uint64_t tmp_rdev = cpu_to_le64(credp->fc_rdev);
-        err = setxattr(path, "user.virtfs.rdev", &tmp_rdev, sizeof(dev_t), 0, 0, 0);
+        err = setxattr(path, "user.virtfs.rdev", &tmp_rdev, sizeof(dev_t), 0, 0);
         if (err) {
             return err;
         }
